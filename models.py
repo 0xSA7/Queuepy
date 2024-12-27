112b9f67d0af5bfd9a445530ebd7a56dd88f347b
mohmaed <mohmaedgomaa15>
2024-12-28 00:28:11 +0200

    Modified some funcs and added ask_user func

--- models.py
+++ models.py
@@ -1,121 +1,164 @@
-from parameter import params
+from parameter import Params
 from math import pow, factorial
+from math import inf
 
 
 ###M/M/1
-class mm1(params):
+class MM1(Params):   
+    """
+    Represents an M/M/1 queuing system.
+    """
 
-    def _init_(self, lumbda, mu):
-        super(lumbda, mu)
+    def __init__(self, lumbda, mu):
+        """
+        Initializes the M/M/1 system parameters.
+        """
+        # Validate inputs
+        if lumbda >= mu:
+            raise ValueError("Arrival rate (lumbda) must be less than service rate (mu) for stability.")
+
+        super().__init__(lumbda, mu)   
 
 
     def findL(self):
-        return self.lumbda // (self.mu - self.lumbda)
-    
+        """Calculates and returns the average number of customers in the system (L)."""
+        return self.lumbda / (self.mu - self.lumbda)   
 
     def findLq(self):
-        return (self.findL() * self.lumbda // self.mu)
-    
+        """Calculates and returns the average number of customers in the queue (Lq)."""
+        return (self.findL() * self.lumbda) / self.mu   
 
     def findW(self):
-         return (1/ (self.mu - self.lumbda))
-    
+        """Calculates and returns the average time a customer spends in the system (W)."""
+        return 1 / (self.mu - self.lumbda)
 
     def findWq(self):
-        return (self.findW() * self.lumbda / self.mu)
-    
+        """Calculates and returns the average time a customer spends in the queue (Wq)."""
+        return (self.findW() * self.lumbda) / self.mu   
 
     def findPk(self, k):
+        """Calculates and returns the probability of having k customers in the system (Pk)."""
         return pow(self.findRu(), k) * (1 - self.findRu())
 
-
     def findRu(self):
-         return self.lumbda / self.mu
+        """Calculates and returns the server utilization (Ru)."""
+        return self.lumbda / self.mu
 
 
 ####M/M/1/K
-class mm1k(params):
+class MM1K(Params):  
+    """
+    Represents an M/M/1/K queuing system.
+    """
 
-   
-    def _init_(self, lumbda, mu, systemcapacuty):
-            super(lumbda, mu, systemcapacuty=systemcapacuty)
-            self._sc = self.systemCapacity
-            self._ru = self.findRu()
-            self._ruK = pow(self._ru, self.systemCapacity)
-            self._ruK1 = pow(self._ru, self.systemCapacity + 1)
+    def __init__(self, lumbda, mu, systemCapacity):
+        """
+        Initializes the M/M/1/K system parameters.
+        """
+        # Validate inputs
+        if lumbda >= mu:
+            raise ValueError("Arrival rate (lumbda) must be less than service rate (mu) for stability.")
+        if systemCapacity <= 0:
+            raise ValueError("System capacity must be a positive integer.")
 
-   
+        super().__init__(lumbda, mu, systemCapacity=systemCapacity)
+        self._sc = systemCapacity
+        self._ru = self.findRu()
+        self._ruK = pow(self._ru, systemCapacity)
+        self._ruK1 = pow(self._ru, systemCapacity + 1)
+
     def findL(self):
-        return self._sc // 2   \
-            if self._ru == 1   \
-                    else self._ru * ((1 - (self._sc + 1) * self._ruK + self._sc * self._ruK1)/ ((1- self._ru)* (1 - self._ruK1)))
-        
+        """Calculates and returns the average number of customers in the system (L)."""
+        if self._ru == 1:
+            return self._sc / 2.0   
+        else:
+            numerator = self._ru * (1 - (self._sc + 1) * self._ruK + self._sc * self._ruK1)
+            denominator = (1 - self._ru) * (1 - self._ruK1)
+            return numerator / denominator
 
     def findLq(self):
+        """Calculates and returns the average number of customers in the queue (Lq)."""
         return self._findLambdaDash() * self.findWq()
-    
 
     def findW(self):
-            return self.findL() / self._findLambdaDash()
-
+        """Calculates and returns the average time a customer spends in the system (W)."""
+        return self.findL() / self._findLambdaDash()
 
     def findWq(self):
-        return  self.findW() - (1.0/self.mu)
-
+        """Calculates and returns the average time a customer spends in the queue (Wq)."""
+        return self.findW() - (1.0 / self.mu)
 
     def findPk(self, k):
-         return 1.0/ (self._sc + 1.0) \
-            if self._ru == 1 \
-                else  self._ruK * ((1 - self._ru)/(1-self._ruK1))
-    
+        """Calculates and returns the probability of having k customers in the system (Pk)."""
+        if self._ru == 1:
+            return 1.0 / (self._sc + 1.0)
+        else:
+            return self._ruK * ((1 - self._ru) / (1 - self._ruK1))
 
     def _findLambdaDash(self):
+        """Calculates and returns the effective arrival rate (lambda_dash)."""
         return self.lumbda * (1 - self.findPk(self._sc))
-     
 
     def findRu(self):
-            return self.lumbda / self.mu
+        """Calculates and returns the server utilization (Ru)."""
+        return self.lumbda / self.mu
 
 
 ###M/M/C
-class mmc(params):
-     
-    def _init_(self, lumbda, mu, numberofServers):
-        super(lumbda, mu, numberOfServers=numberofServers)
-        self.c = numberofServers
-        self.findR = self.lumbda / self.mu
-        self._Lq = self.findLq()
-        self.P0 = self.findP0()
+class MMC(Params):   
+    """
+    Represents an M/M/c queuing system.
+    """
+
+    def __init__(self, lumbda, mu, numberOfServers):
+        """
+        Initializes the M/M/c system parameters.
+        """
+        # Validate inputs
+        if lumbda >= mu * numberOfServers:
+            raise ValueError("Arrival rate (lumbda) must be less than service rate (mu) * number of servers (c) for stability.")
+        if numberOfServers <= 0:
+            raise ValueError("Number of servers must be a positive integer.")
+
+        super().__init__(lumbda, mu, numberOfServers=numberOfServers)
+        self.c = numberOfServers
+        self.findR = self.lumbda / self.mu   
+        self.P0 = self.findP0() 
+        self._Lq = self.findLq()   
+          
 
 
     def findL(self):
-       return self._Lq + self.findR()
-    
+        """Calculates and returns the average number of customers in the system (L)."""
+        return self._Lq + self.findR  
 
     def findLq(self):
-         return ((pow(self.findR(), self.c+1) / self.c) / (factorial(self.c) * (pow( 1 - self.findR() / self.c, 2)))) * self.P0
-    
+        """Calculates and returns the average number of customers in the queue (Lq)."""
+        numerator = (pow(self.findR, self.c + 1) / self.c)
+        denominator = factorial(self.c) * (pow(1 - (self.findR / self.c), 2))
+        return (numerator / denominator) * self.P0
 
     def findW(self):
-         return self._Lq/self.lumbda + 1/self.mu
-    
+        """Calculates and returns the average time a customer spends in the system (W)."""
+        return (self._Lq / self.lumbda) + (1 / self.mu)
 
     def findWq(self):
+        """Calculates and returns the average time a customer spends in the queue (Wq)."""
         return self._Lq / self.lumbda
-    
 
     def findPk(self, k):
-        
-        return  pow(self.lumbda, k)/(factorial(k)* pow(self.mu, k)) * self.P0\
-            if k < self.c \
-                else pow(self.lumbda, k) / (pow(self.c, k - self.c) * factorial(self.c) * pow(self.mu, k)) * self.P0
-
+        """Calculates and returns the probability of having k customers in the system (Pk)."""
+        if k < self.c:
+            return (pow(self.lumbda, k) / (factorial(k) * pow(self.mu, k))) * self.P0
+        else:
+            return (pow(self.lumbda, k) / (pow(self.c, k - self.c) * factorial(self.c) * pow(self.mu, k))) * self.P0
 
     def findRu(self):
-         return self.findR / self.c
-    
-    
+        """Calculates and returns the server utilization (Ru)."""
+        return self.findR / self.c
+
     def findP0(self):
+        """Calculates and returns the probability of having 0 customers in the system (P0)."""
         r = self.findR
         ru = self.findRu()
         c = self.c
@@ -124,64 +167,122 @@ class mmc(params):
         if ru < 1:
             for i in range(c):
                 first += pow(r, i) / factorial(i)
-            return 1/ (first + ((c * pow(r, c))/(factorial(c)*(c - r))))
-        
-        for i in range(c):
-            first += (1/factorial(i)) * pow(r, i)
-        return 1/ (first + ((1/factorial(c)) * pow(r, c) * ((c*self.mu)/(c * self.mu - self.lumbda))))
+            return 1 / (first + ((c * pow(r, c)) / (factorial(c) * (c - r))))
+        else:
+            for i in range(c):
+                first += (1 / factorial(i)) * pow(r, i)
+            return 1 / (first + ((1 / factorial(c)) * pow(r, c) * ((c * self.mu) / (c * self.mu - self.lumbda))))
+
 
 
 ####M/M/C/K
-class mmck(params):
+class MMCK(Params):   
+    """
+    Represents an M/M/c/K queuing system.
+    """
 
-    def _init_(self,lumbda, mu, numberOfServers, systemCapacity):
-         super(lumbda, mu, numberOfServers, systemCapacity)
-         self.c = numberOfServers
-         self.sc = systemCapacity
-         self.findR = self.lumbda / self.mu
-         self.findLambdaDash = self.lumbda * (1 - self.findPk(self.k))
-         self.findp0 = self.findP0()
+    def __init__(self, lumbda, mu, numberOfServers, systemCapacity):
+        """
+        Initializes the M/M/c/K system parameters.
+        """
+        # Validate inputs
+        #if lumbda >= mu * numberOfServers:
+            #raise ValueError("Arrival rate (lumbda) must be less than service rate (mu) * number of servers (c) for stability.")
+        if numberOfServers <= 0:
+            raise ValueError("Number of servers must be a positive integer.")
+        if systemCapacity <= 0:
+            raise ValueError("System capacity must be a positive integer.")
+
+        super().__init__(lumbda, mu, numberOfServers=numberOfServers, systemCapacity=systemCapacity)
+        self.c = numberOfServers
+        self.sc = systemCapacity
+        self.findR = self.lumbda / self.mu   
+        self.findp0 = self.findP0()  
+        self.findLambdaDash = self.lumbda * (1 - self.findPk(self.sc))  
 
     def findL(self):
-        last = 0
-        for i in range(self.c):
-            last += (self.c - i) * (pow(self.findR, i) / factorial(i))
+        """Calculates and returns the average number of customers in the system (L)."""
+        last = sum((self.c - i) * (pow(self.findR, i) / factorial(i)) for i in range(self.c))
         return self.findLq() + self.c - self.findp0 * last
-    
-    
+
     def findLq(self):
-        return (self.findRu() * pow(self.findR, self.c) * self.findp0) \
-            / (factorial(self.c) * pow(1 - self.findRu(), 2))  \
-               * (1 - pow(self.findRu(), self.k - self.c + 1) - (1 - self.findRu()) \
-                  * (self.k - self.c + 1) * pow(self.findRu(), self.k - self.c))
-    
+        """Calculates and returns the average number of customers in the queue (Lq)."""
+        ru = self.findRu()
+        numerator = ru * pow(self.findR, self.c) * self.findp0
+        denominator = factorial(self.c) * pow(1 - ru, 2)
+        blocking_factor = (1 - pow(ru, self.sc - self.c + 1) - (1 - ru) * (self.sc - self.c + 1) * pow(ru, self.sc - self.c))
+        return (numerator / denominator) * blocking_factor
 
     def findW(self):
+        """Calculates and returns the average time a customer spends in the system (W)."""
         return self.findL() / self.findLambdaDash
-    
 
     def findWq(self):
+        """Calculates and returns the average time a customer spends in the queue (Wq)."""
         return self.findLq() / self.findLambdaDash
-    
 
     def findPk(self, n):
+        """Calculates and returns the probability of having n customers in the system (Pk)."""
         if n < self.c:
             return (pow(self.findR, n) / factorial(n)) * self.findp0
-        return (pow(self.findR, n) / (pow(self.c, n-self.c) * factorial(self.c))) * self.findp0
-
+        return (pow(self.findR, n) / (pow(self.c, n - self.c) * factorial(self.c))) * self.findp0
 
     def findP0(self):
-        first = 0
-        for i in range(self.c):
-            first += pow(self.findR, i) / factorial(i)
-            
-        return 1 / (first + (pow(self.findR, self.c) / factorial(self.c)) \
-                        * ((1 - pow(self.findRu(), self.k - self.c + 1)) / (1- self.findRu()))) \
-                        if self.findRu() != 1  \
-                         else 1 / (first + (pow(self.findR, self.c) / factorial(self.c)) * (self.k - self.c + 1));
-
+        """Calculates and returns the probability of having 0 customers in the system (P0)."""
+        first = sum(pow(self.findR, i) / factorial(i) for i in range(self.c))
+        ru = self.findRu()
+        if ru != 1:
+            return 1 / (first + (pow(self.findR, self.c) / factorial(self.c)) * ((1 - pow(ru, self.sc - self.c + 1)) / (1 - ru)))
+        else:
+            return 1 / (first + (pow(self.findR, self.c) / factorial(self.c)) * (self.sc - self.c + 1))
 
     def findRu(self):
-         return  self.findR / self.c
+        """Calculates and returns the server utilization (Ru)."""
+        return self.findR / self.c
 
-   
+    
+def solution(lumbda, mu, numberOfServers=1, systemCapacity=inf):
+         
+      if numberOfServers == 1:
+        if systemCapacity == inf:
+          MM1(lumbda, mu).display()
+        else: MM1K(lumbda, mu, systemCapacity).display()
+      
+      else: 
+        if systemCapacity == inf:
+          MMC(lumbda, mu, numberOfServers).display()
+        else: MMCK(lumbda, mu, numberOfServers, systemCapacity).display()
+        
+
+def ask_user():
+    
+    model = input("Enter the model you need (MM1, MM1K, MMC, MMCK): ")
+
+    # Ask for parameters based on the chosen model
+    if model == "MM1":
+        arrival_rate = float(input("Enter the arrival rate (lambda): "))
+        service_rate = float(input("Enter the service rate (mu): "))
+        solution(arrival_rate, service_rate)
+
+    elif model == "MM1K":
+        arrival_rate = float(input("Enter the arrival rate (lambda): "))
+        service_rate = float(input("Enter the service rate (mu): "))
+        capacity = int(input("Enter the system capacity (k): "))
+        solution(arrival_rate, service_rate, systemCapacity=capacity)
+
+    elif model == "MMC":
+        arrival_rate = float(input("Enter the arrival rate (lambda): "))
+        service_rate = float(input("Enter the service rate (mu): "))
+        servers = int(input("Enter the Number of Servers (c):"))
+        solution(arrival_rate, service_rate, numberOfServers=servers)
+
+    elif model == "MMCK":
+        arrival_rate = float(input("Enter the arrival rate (lambda): "))
+        service_rate = float(input("Enter the service rate (mu): "))
+        servers = int(input("Enter the Number of Servers (c):"))
+        capacity = int(input("Enter the system capacity (k): "))
+        solution(arrival_rate, service_rate, servers, capacity)
+
+    else:
+        print("Invalid model selection.")
+
--- parameter.py
+++ parameter.py
@@ -1,33 +1,54 @@
 from math import inf
+from numpy import random
 
-class params:
-     
-    
-    def set(self, lumbda, mu,numberOfServers = 1 , systemCapacity = inf):
-            self.lumbda = lumbda
-            self.mu = mu
-            self.numberOfServers = numberOfServers
-            self.systemCapacity = systemCapacity
+class Params:
+    """
+    Represents parameters for a queuing system.
+    This class serves as an abstract base class for specific queuing models.
+    """
 
-    def findL():
-          pass 
-      
-    def findLq():
-         pass
-    
-    def findW():
-          pass
-    
-    def findWq():
-          pass
-    
-    def findPk(k):
-          pass
-    
-    def findRu():
-          pass
-    
+    def __init__(self, lumbda, mu, numberOfServers=1, systemCapacity=inf):
+        """
+        Initializes the parameters.
+        """
+        self.lumbda = lumbda
+        self.mu = mu
+        self.numberOfServers = numberOfServers
+        self.systemCapacity = systemCapacity
+
+    def findL(self):
+        """Calculates and returns the average number of customers in the system (L)."""
+        raise NotImplementedError("This method should be implemented in subclasses.")
+
+    def findLq(self):
+        """Calculates and returns the average number of customers in the queue (Lq)."""
+        raise NotImplementedError("This method should be implemented in subclasses.")
+
+    def findW(self):
+        """Calculates and returns the average time a customer spends in the system (W)."""
+        raise NotImplementedError("This method should be implemented in subclasses.")
+
+    def findWq(self):
+        """Calculates and returns the average time a customer spends in the queue (Wq)."""
+        raise NotImplementedError("This method should be implemented in subclasses.")
+
+    def findPk(self, k):
+        """Calculates and returns the probability of having k customers in the system (Pk)."""
+        raise NotImplementedError("This method should be implemented in subclasses.")
+
+    def findRu(self):
+        """Calculates and returns the server utilization (Ru)."""
+        raise NotImplementedError("This method should be implemented in subclasses.")
+
     def display(self):
-          print("L = {} -- Lq = {} -- W = {} -- Wq = {}".format(self.findL(), self.findLq, self.findW, self.findWq))
+        """Displays the calculated performance measures."""
+        print(f" L = {self.findL()} \n Lq = {self.findLq()} \n W = {self.findW()} \n Wq = {self.findWq()}")
 
-    
+    def generate_random_params(self, num_customers):
+
+        p1 = random.exponential(scale=self.lumbda, size=num_customers)
+        p2 = random.exponential(scale=self.mu, size=num_customers)
+
+        return p1, p2
+
+        
